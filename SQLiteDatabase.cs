8b34eb8f5a88f9992d1a30dbf916921db5e5df03
Devon Weaver <devon.weaver@gmail.com>
2012-04-29 22:44:22 -0400

    finshes the implementation of the class. commented all methods

--- SQLiteDatabase.cs
+++ SQLiteDatabase.cs
@@ -26,6 +26,7 @@
 using System;
 using System.Data.SQLite;
 using System.Data;
+using System.Collections.Generic;
 
 namespace Com.Maadworks.BattleForEurope
 {
@@ -111,6 +112,140 @@ namespace Com.Maadworks.BattleForEurope
 				return (value != null) ? value.ToString() : "";
 			}
 			
+		}
+		
+		/// <summary>
+		/// Update the database
+		/// </summary>
+		/// <param name='tableName'>
+		/// table's name.
+		/// </param>
+		/// <param name='data'>
+		/// data to be updated
+		/// </param>
+		/// <param name='where'>
+		/// the conditions.
+		/// </param>
+		public bool Update(string tableName, Dictionary<string, string> data, string where) {
+			
+			string vals = "";
+			bool returnCode = true;
+			if (data.Count >= 1) 
+			{
+				foreach (KeyValuePair<string, string> val in data) {
+					vals += String.Format(" {0} = '{1}',", val.Key.ToString(), val.Value.ToString());
+		
+				}
+				vals = vals.Substring(0, vals.Length - 1);
+			}
+			
+			try 
+			{
+				this.ExecuteNonQuery(String.Format("update {0} set {1} where {2};", tableName, vals, where));	
+			}
+			catch
+			{
+				returnCode = false;
+			}
+			return returnCode;
+		}
+		
+		/// <summary>
+		/// Delete the specified record from tableName and where.
+		/// </summary>
+		/// <param name='tableName'>
+		/// table name.
+		/// </param>
+		/// <param name='where'>
+		///  where.
+		/// </param>
+		public bool Delete(string tableName, string where) {
+			
+			bool returnCode = true;
+			try 
+			{
+				this.ExecuteNonQuery(String.Format("delete from {0} where {1};", tableName, where));
+				
+			}
+			catch (Exception fail)
+			{
+				Console.WriteLine(fail.Message);
+				returnCode = false;
+			}
+			return returnCode;
+			
+			
+		}
+		
+		/// <summary>
+		/// Insert the specified tableName and data.
+		/// </summary>
+		/// <param name='tableName'>
+		/// If set to <c>true</c> table name.
+		/// </param>
+		/// <param name='data'>
+		/// If set to <c>true</c> data.
+		/// </param>
+		public bool Insert(string tableName, Dictionary<string, string> data)
+		{
+			string columns = "";
+			string values = "";
+			bool returnCode = true;
+			
+			foreach (KeyValuePair<string, string> val in data)
+			{
+				columns += String.Format(" {0},", val.Key.ToString());
+				values += String.Format (" '{0}',", val.Value);
+			}
+			
+			columns = columns.Substring(0, columns.Length - 1);
+			values = values.Substring(0, values.Length -1);
+			
+			try 
+			{
+				this.ExecuteNonQuery(String.Format("insert into {0}({1}) values({2});", tableName, columns, values));	
+			}
+			catch (Exception fail) 
+			{
+				Console.WriteLine(fail.Message);
+				returnCode = false;
+				
+			}
+			
+			return returnCode;
+		
+		}
+		
+		public bool ClearDB()
+		{
+			DataTable tables;
+			
+			try
+			{
+				tables = this.GetDataTable("select NAME from SQLITE_MASTER where type='table' order by NAME;");
+				foreach (DataRow table in tables.Rows)
+				{
+					this.ClearTable(table["NAME"].ToString());
+				}
+				return true;
+			}
+			catch 
+			{
+				return false;
+			}
+		}
+		
+		public bool ClearTable(string table) 
+		{
+			try
+			{
+				this.ExecuteNonQuery(String.Format("delete from {0};", table));
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
 		}
 	}
 }
